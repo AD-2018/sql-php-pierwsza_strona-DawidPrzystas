60c879bf2bd76cddbc0f6da39e5fdf5831637a65
DawidPrzystas <75605407+DawidPrzystas@users.noreply.github.com>
2020-12-07 16:19:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php
-echo("test333");
 
+$servername = "mysql-dawidxdxdxd.alwaysdata.net" ;
+$username = "root" ;
+$password = "" ;
+$dbname = "dawid" ;
+
+$conn = new mysqli($servername, $username, $password, $dbname) ;
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error) ;
+}
+echo "Connected successfully" ;
+
+$sql = "SELECT * FROM pracownicy";
+$result = $conn->query($sql);
+
+
+ while($row = $result->fetch_assoc()) {
+	echo "imie: " . $row["imie"]. "zarobki: " . $row["zarobki"]. "<br>";
+ }
+
+$conn->close();
 ?>
