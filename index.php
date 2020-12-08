5819c58096755891cd38ca029c458c591596c864
Tomjand <domagala.tomasz@gmail.com>
2020-12-08 17:55:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,9 +13,31 @@ if ($conn->connect_error) {
 echo "Connected successfully" ;
 
 $sql = "SELECT * FROM pracownicy";
+echo("<li> zad wprowadzenie".$sql);
 $result = $conn->query($sql);
+echo("<table border='1'>");
+echo("<tr>");
+echo("
+				<th>id</th>
+				<th>Imie</th>
+				<th>nazwa dzialu</th>
+				<th>zarobki</th>");
+echo("</tr>");
+  
+  while($row = $result->fetch_assoc()) {
+			echo("<tr>");    
+					echo( "<td>".$row['id']."</td>" );
+					echo( "<td>".$row['imie']."</td>" );
+					echo( "<td>".$row['dzial']."</td>" );
+					echo( "<td>".$row['zarobki']."</td>" );
+			echo("</tr>");
+  }
+echo("</table>");
 
 
+$sql = "SELECT  * FROM pracownicy WHERE dzial=2";
+echo("<li> zad 1".$sql);
+$result = $conn->query($sql);
 echo("<table border='1'>");
 echo("<tr>");
 echo("
