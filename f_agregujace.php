31f245c66bacc6d3a42c8b69d1b5c56e80fd6e5a
DawidPrzystas <75605407+DawidPrzystas@users.noreply.github.com>
2020-12-13 18:24:30 +0100

    Update f_agregujace.php

--- f_agregujace.php
+++ f_agregujace.php
@@ -2,4 +2,45 @@
 
 echo("jest w pliku f.agregujace");
 
+
+$servername = "mysql-dawidxdxdxd.alwaysdata.net" ;
+$username = "220777" ;
+$password = "turbosecretpassword" ;
+$dbname = "dawidxdxdxd_pracownicy" ;
+
+$conn = new mysqli($servername, $username, $password, $dbname) ;
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error) ;
+}
+echo "Connected successfully</br>" ;
+
+
+/*===zad1====*/
+$sql = "SELECT SUM(zarobki) FROM pracownicy";
+echo("<li> zad 4 ".$sql);
+$result = $conn->query($sql);
+echo("<table border='1'>");
+echo("<tr>");
+echo("
+				<th>id</th>
+				<th>Imie</th>
+				<th>nazwa dzialu</th>
+				<th>zarobki</th>");
+echo("</tr>");
+  
+  while($row = $result->fetch_assoc()) {
+			echo("<tr>");    
+					echo( "<td>".$row['id_pracownicy']."</td>" );
+					echo( "<td>".$row['imie']."</td>" );
+					echo( "<td>".$row['nazwa_dzial']."</td>" );
+					echo( "<td>".$row['zarobki']."</td>" );
+			echo("</tr>");
+  }
+echo("</table>");
+
+
+
+$conn->close();
+
 ?>
