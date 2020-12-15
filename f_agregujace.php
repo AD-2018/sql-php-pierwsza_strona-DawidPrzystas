2e9daf239558d53ac34afc5a29b206f3a7cb392d
DawidPrzystas <75605407+DawidPrzystas@users.noreply.github.com>
2020-12-15 20:58:32 +0100

    Update f_agregujace.php

--- f_agregujace.php
+++ f_agregujace.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Moje Marne Wypociny</title>
+  </head>
+  <body>
+    <h1>Funkcje Agregujace</h1>
+    <div>
+      <a href="index.php">powrot</a>
+    </div>
+
 <?php
 
 echo("jest w pliku f.agregujace</br>");
@@ -58,3 +72,5 @@ echo("</table>");
 $conn->close();
 
 ?>
+  </body>
+</html>
