30d46bbe27e1207443fcb2656129dd78f10fe563
DawidPrzystas <75605407+DawidPrzystas@users.noreply.github.com>
2020-12-17 16:46:56 +0100

    Update orgprac.php

--- orgprac.php
+++ orgprac.php
@@ -14,16 +14,8 @@
 
 <?php
 
-$servername = "mysql-dawidxdxdxd.alwaysdata.net" ;
-$username = "220777" ;
-$password = "turbosecretpassword" ;
-$dbname = "dawidxdxdxd_pracownicy" ;
+    require_once("connect.php");
 
-$conn = new mysqli($servername, $username, $password, $dbname) ;
-
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error) ;
-}
 echo "Connected successfully</br>" ;
 ?>
 </body>
