7cd5236d5a3eded68bb4f3d81fdaf34da051bd63
DawidPrzystas <75605407+DawidPrzystas@users.noreply.github.com>
2020-12-17 17:28:17 +0100

    Update menu.php

--- menu.php
+++ menu.php
@@ -2,9 +2,10 @@
 
 echo('
 
-<a href="https://github.com/AD-2018/sql-php-pierwsza_strona-DawidPrzystas">Github</a>
-<a href="index.php">Strona główna</a>
-<a href="f_agregujace.php">Funkcje Agregujace</a>
+<a href="https://github.com/AD-2018/sql-php-pierwsza_strona-DawidPrzystas">Github</br></a>
+<a href="index.php">Strona główna</br></a>
+<a href="f_agregujace.php">Funkcje Agregujace</br></a>
+<a href="orgprac.php">Data i Czas</br></a>
 ');
 
 
