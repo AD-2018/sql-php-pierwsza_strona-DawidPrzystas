4253081afd2aaf2125e4684322f558b2ce51d6f8
Tomjand <domagala.tomasz@gmail.com>
2020-12-17 16:40:44 +0100

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "mysql-dawidxdxdxd.alwaysdata.net" ;
+$username = "220777" ;
+$password = "turbosecretpassword" ;
+$dbname = "dawidxdxdxd_pracownicy" ;
+
+$conn = new mysqli($servername, $username, $password, $dbname) ;
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error) ;
+}
+echo "Connected successfully</br>" ;
+
+?>
