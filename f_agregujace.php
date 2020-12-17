8c87be0cccdfe751729726f39f4f2760a570d6e6
DawidPrzystas <75605407+DawidPrzystas@users.noreply.github.com>
2020-12-17 16:46:26 +0100

    Update f_agregujace.php

--- f_agregujace.php
+++ f_agregujace.php
@@ -14,16 +14,7 @@
 
 <?php
 
-$servername = "mysql-dawidxdxdxd.alwaysdata.net" ;
-$username = "220777" ;
-$password = "turbosecretpassword" ;
-$dbname = "dawidxdxdxd_pracownicy" ;
-
-$conn = new mysqli($servername, $username, $password, $dbname) ;
-
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error) ;
-}
+require_once("connect.php");
 echo "Connected successfully</br>" ;
 
 /*===zad1====*/
