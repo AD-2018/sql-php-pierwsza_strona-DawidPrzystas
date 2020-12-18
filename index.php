9fb9e050f98d7463ae6b2825291deb63c1e608d5
DawidPrzystas <75605407+DawidPrzystas@users.noreply.github.com>
2020-12-18 10:27:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -38,32 +38,32 @@ echo("</tr>");
 echo("</table>");
 
 /*===zad1====*/
-	<!--  function zapytanie(){
-	 	require_once("connect.php");
-		  $sql = "SELECT * FROM `pracownicy` , `organizacja` WHERE dzial = id_org";
-		echo("<li> 1 </br> Funkcja: ".$sql);
-		$result = $conn->query($sql);
-		echo("<table border='1'>");
-		echo("<tr>");
-		echo("
-				<th>id</th>
-				<th>Imie</th>
-				<th>nazwa dzialu</th>
-				<th>zarobki</th>");
-		echo("</tr>");
+	#  function zapytanie(){
+	# 	require_once("connect.php");
+	#	  $sql = "SELECT * FROM `pracownicy` , `organizacja` WHERE dzial = id_org";
+	#	echo("<li> 1 </br> Funkcja: ".$sql);
+	#	$result = $conn->query($sql);
+	#	echo("<table border='1'>");
+	#	echo("<tr>");
+	#	echo("
+	#			<th>id</th>
+	#			<th>Imie</th>
+	#			<th>nazwa dzialu</th>
+	#			<th>zarobki</th>");
+	#	echo("</tr>");
   
-  		while($row = $result->fetch_assoc()) {
-			echo("<tr>");    
-					echo( "<td>".$row['id_pracownicy']."</td>" );
-					echo( "<td>".$row['imie']."</td>" );
-					echo( "<td>".$row['nazwa_dzial']."</td>" );
-					echo( "<td>".$row['zarobki']."</td>" );
-			echo("</tr>");
-  		}
-		echo("</table>");  
-	  }
-	  zapytanie();
-	  -->
+  	#	while($row = $result->fetch_assoc()) {
+	#		echo("<tr>");    
+	#				echo( "<td>".$row['id_pracownicy']."</td>" );
+	#				echo( "<td>".$row['imie']."</td>" );
+	#				echo( "<td>".$row['nazwa_dzial']."</td>" );
+	#				echo( "<td>".$row['zarobki']."</td>" );
+	#		echo("</tr>");
+  	#	}
+	#	echo("</table>");  
+	#}
+	#zapytanie();
+	  
 	  
 	  
 $sql = "SELECT * FROM `pracownicy` , `organizacja` WHERE dzial = id_org";
