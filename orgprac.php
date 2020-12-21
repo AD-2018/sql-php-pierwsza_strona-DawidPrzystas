5d4e0aa142ced2aae88f7cda72d55a9908ba7159
DawidPrzystas <75605407+DawidPrzystas@users.noreply.github.com>
2020-12-21 11:43:43 +0100

    Update orgprac.php

--- orgprac.php
+++ orgprac.php
@@ -35,28 +35,39 @@ echo("</tr>");
 echo("</table>");
 	  
 	  
-	  /*===zad1====*/
-	  function zapytanie($sql){
-	 	require("connect.php");
+	 /*===zad1====*/
+	  function zapytanie($sql,$conn){
+	 	/*require("connect.php");*/
 		  //$sql = "SELECT * FROM `pracownicy` , `organizacja` WHERE dzial = id_org";
-		echo("<li> 111 </br> Funkcja: ".$sql);
+		echo("<h2>Zad. 1 : Pracownicy tylko z działu 1 i 4</h4>");
+		echo("<li> Funkcja: ".$sql);
 		$result = $conn->query($sql);
+		//zwraca błąd jeśli zapytanie jest nieprawidłowo zapisane
+		if ( $result) {
+		      echo "<li> zapytanie wykonało się prawidłowo, wszystko ok";
+		} else {
+		      echo "Uwaga Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
 		echo("<table border='1'>");
 		echo("<tr>");
 		echo("
-				<th>imie</th>
-				<th>wiek</th>");
+				<th>id</th>
+				<th>Imie</th>
+				<th>nazwa dzialu</th>
+				<th>zarobki</th>");
 		echo("</tr>");
   
   		while($row = $result->fetch_assoc()) {
 			echo("<tr>");    
+					echo( "<td>".$row['id_pracownicy']."</td>" );
 					echo( "<td>".$row['imie']."</td>" );
-					echo( "<td>".$row['wiek']."</td>" );
+					echo( "<td>".$row['nazwa_dzial']."</td>" );
+					echo( "<td>".$row['zarobki']."</td>" );
 			echo("</tr>");
   		}
 	echo("</table>");  
 	}
-	zapytanie("SELECT * ,YEAR(curdate())-YEAR(data_urodzenia) AS wiek FROM pracownicy, organizacja WHERE nazwa_dzial='serwis'");
+	zapytanie("SELECT * FROM `pracownicy` , `organizacja` WHERE dzial = id_org", $conn);
 	  
 	  
 
