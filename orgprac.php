4ba9415f94c8cb4d9df9febb5b8b7114498af02d
DawidPrzystas <75605407+DawidPrzystas@users.noreply.github.com>
2020-12-21 11:41:24 +0100

    Update orgprac.php

--- orgprac.php
+++ orgprac.php
@@ -59,38 +59,6 @@ echo("</table>");
 	zapytanie("SELECT * ,YEAR(curdate())-YEAR(data_urodzenia) AS wiek FROM pracownicy, organizacja WHERE nazwa_dzial='serwis'");
 	  
 	  
-	  function zapytanie($sql,$conn){
-	 	/*require("connect.php");*/
-		  //$sql = "SELECT * FROM `pracownicy` , `organizacja` WHERE dzial = id_org";
-		echo("<h2>Zad. 3 : Suma lat wszystkich pracowników</h4>");
-		echo("<li> Funkcja: ".$sql);
-		$result = $conn->query($sql);
-		//zwraca błąd jeśli zapytanie jest nieprawidłowo zapisane
-		if ( $result) {
-		      echo "<li> zapytanie wykonało się prawidłowo, wszystko ok";
-		} else {
-		      echo "Uwaga Error: " . $sql . "<br>" . mysqli_error($conn);
-		}
-		echo("<table border='1'>");
-		echo("<tr>");
-		echo("
-				<th>id</th>
-				<th>Imie</th>
-				<th>nazwa dzialu</th>
-				<th>zarobki</th>");
-		echo("</tr>");
-  
-  		while($row = $result->fetch_assoc()) {
-			echo("<tr>");    
-					echo( "<td>".$row['id_pracownicy']."</td>" );
-					echo( "<td>".$row['imie']."</td>" );
-					echo( "<td>".$row['nazwa_dzial']."</td>" );
-					echo( "<td>".$row['zarobki']."</td>" );
-			echo("</tr>");
-  		}
-	echo("</table>");  
-	}
-	
 
 
 ?>
